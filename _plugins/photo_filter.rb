38d0a4d446c0ae68bfa39605309354f929338654
Luca Danelutti <lucadanelutti@outlook.com>
2021-05-24 22:14:40 +0200

    Sort photos by name

--- _plugins/photo_filter.rb
+++ _plugins/photo_filter.rb
@@ -5,8 +5,8 @@ module Jekyll
     def photo_filter(files)
       photos = files.select {|photo| photo.relative_path.include?("original") }
       photos.sort_by do |photo|
-        EXIFR::JPEG.new(photo.path).date_time.to_s
-      end.reverse
+        photo.name
+      end
     end
   end
 end
